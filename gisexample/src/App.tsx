9e9c4bb8484394688090872ea5a43f3a02a23c2c
GAGvozdik <69218434+GAGvozdik@users.noreply.github.com>
2026-02-16 12:52:27 +0100

    go to working variant

--- gisexample/src/App.tsx
+++ gisexample/src/App.tsx
@@ -44,8 +44,80 @@ import FolderIcon from '@mui/icons-material/Folder';
 import { useState } from 'react';
 import RectangleOutlinedIcon from '@mui/icons-material/RectangleOutlined';
 
-import Graph from './components/Graph';
-import CustomAppBar from './components/CustomAppBar';
+
+const drawerWidth = 350;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+
+const AppBar = styled(MuiAppBar, 
+  {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
 
 function App() {
 
@@ -88,35 +160,187 @@ function App() {
     <div className="App" >
 
       <ThemeProvider theme={theme}>
-     
-      <CustomAppBar>
-          
+      <Box>
 
-      {pr == 'WGS84' ? (
+      <AppBar position="fixed" open={open} style={{ backgroundColor: '#318DC2' }}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-        <div>
-          <MainMap />  
-          {/* <IndexPage /> */}
-        </div>
+          <Typography variant="h6" noWrap component="div">
+            WGS84 to Lambert conic
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      
+    <Drawer
+      variant="permanent"
+      open={open}
+      
+    >
+
+        <DrawerHeader >
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+
+
+        <List>
+          {/* ['Lambert Conic', 'WGS84', 'Catalog'] */}
+            
+            <ListItem key={'Lambert Conic'} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={() => handleListItemClick('Lambert Conic')} // Добавляем обработчик клика
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+              
+                  <CircleOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Lambert Conic'} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+         
+            
+            <ListItem key={'WGS84'} disablePadding>
+              <ListItemButton
+              onClick={() => handleListItemClick('WGS84')} // Добавляем обработчик клика
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  
+                 <RectangleOutlinedIcon />
+                </ListItemIcon>
+                <ListItemText primary={'WGS84'} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+         
+
+            <ListItem key={'Lambert Conic Zone'} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+              onClick={() => handleListItemClick('Lambert Conic Zone')} // Добавляем обработчик клика
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  
+                  <ChangeHistoryIcon />
+                </ListItemIcon>
+                <ListItemText primary={'Lambert Conic'} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+
+
+            {/* <ListItem key={'Catalog'} disablePadding sx={{ display: 'block' }} >
+            
+              <Divider />
+
+              {isTree == false ? <BasicSimpleTreeView /> : 
+                    <> 
+                      <ListItemButton
+                      onClick={() => setIsTree(!isTree)} // Добавляем обработчик клика
+                        sx={{
+                          minHeight: 48,
+                          justifyContent: open ? 'initial' : 'center',
+                          px: 2.5,
+                        }}
+                      >
+                        <ListItemIcon
+                          sx={{
+                            minWidth: 0,
+                            mr: open ? 3 : 'auto',
+                            justifyContent: 'center',
+                          }}
+                        >
+                          <FolderIcon onClick={handleDrawerOpen}/>
+                        </ListItemIcon>
+                      </ListItemButton>
+                    </>
+                  }
+
+            </ListItem> */}
+         
+
+            
+
+      <Divider />
+      </List>
+  
+      </Drawer>
+      <Box component="main" >
+        <DrawerHeader/>
+        
+
+        {/* <MainMap theme={theme} /> */}
+
+        {pr == 'WGS84' ? (
+
+          <div>
+            <MainMap />  
+            {/* <IndexPage /> */}
+          </div>
 
         ) : (
-
-        <div>
-          { pr == 'Lambert Conic' ? (
-              <div>
-                <LambConic />
-              </div>
-            ) : (
-              <div>
-                <MapWithPolygons /> 
-              </div>
-            )
-          } 
           
-        </div>
+          <div>
+            { pr == 'Lambert Conic' ? (
+                <div>
+                  <LambConic />
+                </div>
+              ) : (
+                <div>
+                  <MapWithPolygons /> 
+                </div>
+              )
+            } 
+            
+          </div>
         )}
-      </CustomAppBar>
-
+        
+      </Box>
+    </Box>
 
       </ThemeProvider>
     </div>
